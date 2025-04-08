3906942a28f28030c1f49b7fc0b140a594952fc2
Alireza Sobhanian <alireza.sobhanian.dev@gmail.com>
2025-04-09 03:14:43 +0330

    content script copletely implemented for web app

--- background.js
+++ background.js
@@ -1,47 +1,47 @@
 const iconClick = () => {
-    chrome.tabs.create({
-        url: "https://malltina-difference-calculator.vercel.app",
-    });
+	chrome.tabs.create({
+		url: "https://malltina-difference-calculator.vercel.app",
+	});
 };
 
 if (chrome.action?.onClicked) {
-    chrome.action.onClicked.addListener(iconClick);
+	chrome.action.onClicked.addListener(iconClick);
 } else {
-    console.error("chrome.action.onClicked API not available");
+	console.error("chrome.action.onClicked API not available");
 }
 let fetchSignal = new AbortController();
 const fetchData = (requestArr) => {
-    return new Promise((resolve, reject) => {
-        fetchSignal.abort();
-        fetchSignal = new AbortController();
-        const { signal } = fetchSignal;
-        Promise.all(
-            requestArr.map(([url, options]) =>
-                fetch(url, { ...options, signal })
-            )
-        )
-            .then(async (res) => {
-                const resArr = await Promise.all(
-                    res.map((result) => result.json())
-                );
-                return resArr;
-            })
-            .then((res) => resolve(res))
-            .catch((err) => {
-                reject({
-                    name: err.name,
-                    message: err.message,
-                });
-            });
-    });
+	return new Promise((resolve, reject) => {
+		fetchSignal.abort();
+		fetchSignal = new AbortController();
+		const { signal } = fetchSignal;
+		Promise.all(
+			requestArr.map(([url, options]) =>
+				fetch(url, { ...options, signal }),
+			),
+		)
+			.then(async (res) => {
+				const resArr = await Promise.all(
+					res.map((result) => result.json()),
+				);
+				return resArr;
+			})
+			.then((res) => resolve(res))
+			.catch((err) => {
+				reject({
+					name: err.name,
+					message: err.message,
+				});
+			});
+	});
 };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "fetch") {
-        console.log(request.requestArr);
-        fetchData(request.requestArr)
-            .then((res) => sendResponse({ result: res }))
-            .catch((err) => sendResponse({ err }));
-        return true; // Keep channel open for async response
-    }
+	if (request.action === "fetch") {
+		console.log(request.requestArr);
+		fetchData(request.requestArr)
+			.then((res) => sendResponse({ res }))
+			.catch((err) => sendResponse({ err }));
+		return true; // Keep channel open for async response
+	}
 });
--- contentScript.js
+++ contentScript.js
@@ -1,51 +1,58 @@
-document
-    .querySelector("#root")
-    .insertAdjacentHTML("beforeend", '<button class="btn">fetch</button>');
+let reqSign = null;
+let reqOrigin = "";
 
-document.querySelector(".btn").addEventListener("click", () => {
-    chrome.runtime.sendMessage(
-        {
-            action: "fetch",
-            requestArr: AsiaFetchRequest,
-        },
-        (response) => {
-            if (response.err) {
-                console.error(response.err.name || "Error");
-                console.error(response.err.message || "Error Message !");
-            } else {
-                console.log("Computed amount:", response.result);
-            }
-        }
-    );
+(() => {
+	const extensionMarker = document.createElement("div");
+	extensionMarker.id = "__cors_extension_marker";
+	extensionMarker.style.display = "none";
+	document.documentElement.appendChild(extensionMarker);
+	window.postMessage(
+		{
+			type: "EXTENSION_STATUS",
+			bypassCors: true,
+		},
+		"*",
+	);
+})();
+
+window.addEventListener("message", (e) => {
+	if (e.data.type !== "from_web_app") return;
+	const requests = e.data.payload;
+	reqSign = e.data.reqSign;
+	reqOrigin = e.origin;
+	fetchReqToBack(requests);
 });
-document.addEventListener("fetchBC", (e) => {
-    alert("khello" + e.detail?.name);
-});
-function computeAsiaFetch(fetchBody) {
-    return [
-        "https://api.malltina.com/api/v1/asia-shop/compute-cost",
-        {
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify(fetchBody),
-            method: "POST",
-        },
-    ];
-}
-function AsiaFetchBody(country, price, weight) {
-    this.country = country;
-    this.price = price;
-    this.weight = weight;
-}
-const reqArray = [
-    ["china", "30", 700],
-    ["uae", "80", 900],
-];
-const makeRequest = (reqArray, requestBuilder, BodyBuilder) => {
-    return reqArray.map((req) => {
-        return requestBuilder(new BodyBuilder(...req));
-    });
+const sendResToApp = (message, origin) => {
+	window.postMessage(message, origin);
 };
 
-const AsiaFetchRequest = makeRequest(reqArray, computeAsiaFetch, AsiaFetchBody);
+const fetchReqToBack = (requests) => {
+	chrome.runtime.sendMessage(
+		{
+			action: "fetch",
+			requestArr: requests,
+		},
+		(response) => {
+			if (response.err) {
+				const errorName = response.err.name;
+				console.error(errorName || "Error");
+				sendResToApp(
+					{
+						type: "from_extension",
+						reqSign,
+						fulfilled: false,
+						message: errorName,
+					},
+					reqOrigin,
+				);
+			} else {
+				sendResToApp({
+					type: "from_extension",
+					reqSign,
+					fulfilled: true,
+					payload: response.res,
+				});
+			}
+		},
+	);
+};
